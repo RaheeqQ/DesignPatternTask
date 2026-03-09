da8fe9160594fb5aeb828f463b81d49fe8f95c0d
Raheeq Qassrawi <qassrawiraheeq@gmail.com>
2026-03-09 10:44:26 +0200

    fix: let decorator pattern more pythonic

--- p8_endpoint_cross_cutting_concerns/main.py
+++ p8_endpoint_cross_cutting_concerns/main.py
@@ -1,38 +1,37 @@
 #decorator method
-class RESTHandler:
-    def handle_request(self):
-        pass
-
-
-class AuthenticationHandler(RESTHandler):
-    def __init__(self, wrapped):
-        self._wrapped = wrapped
-    def handle_request(self):
+def authentication(handler):
+    # *args collects all positional arguments into a tuple test(1, 2, 3).
+    # **kwargs collects all keyword arguments into a dictionary test(name="Ali", age=20).
+    def wrapped(*args, **kwargs):
         print("Validating auth")
-        self._wrapped.handle_request()
+        result = handler(*args, **kwargs)
+        print("authentication ended successfully")
+        return result
+    return wrapped
 
 
-class RateLimitsHandler(RESTHandler):
-    def __init__(self, wrapped):
-        self._wrapped = wrapped
-    def handle_request(self):
+def rate_limit(handler):
+    def wrapped(*args, **kwargs):
         print("Checking rate limits")
-        self._wrapped.handle_request()
+        result = handler(*args, **kwargs)
+        print("Checking rate limits ended successfully")
+        return result
+    return wrapped
 
-
-class LogHandler(RESTHandler):
-    def __init__(self, wrapped):
-        self._wrapped = wrapped
-    def handle_request(self):
+def log(handler):
+    def wrapped(*args, **kwargs):
         print("Logging request")
-        self._wrapped.handle_request()
+        result = handler(*args, **kwargs)
+        print("Logging ended successfully")
+        return result
+    return wrapped
 
-
-class ProcessHandler(RESTHandler):
-    def handle_request(self):
-        print("Processing request")
+@authentication
+@rate_limit
+@log
+def handle_request():
+    print("Processing request")
 
 
 if __name__ == '__main__':
-    handle = AuthenticationHandler(RateLimitsHandler(LogHandler(ProcessHandler())))
-    handle.handle_request()
+    handle_request()
